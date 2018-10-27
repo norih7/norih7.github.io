f68c723e5e539205feffcb411be3d333d336ec19
norih <goldenbell225@gmail.com>
2018-10-27 13:13:29 +0900

    :heavy_plus_sign: .envの追加 :art: gatsbyのwebpack設定を修正

--- packages/brush-clover/gatsby-node.js
+++ packages/brush-clover/gatsby-node.js
@@ -1,5 +1,8 @@
 var path = require("path");
 var commonPath = "node_modules/common";
+var dotenv = require("dotenv").config({
+    path: path.join(__dirname, "../../.env")
+});
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
     actions.setWebpackConfig({
