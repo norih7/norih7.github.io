82b128f73738aeb1ab84ce41a137d732ac090738
norih <goldenbell225@gmail.com>
2019-01-16 20:49:08 +0900

    :art: デザイン調整 :bug ブログエントリー作成のシェルスクリプ:art: デザイン調整

--- packages/norih/src/scripts/addEntry/index.sh
+++ packages/norih/src/scripts/addEntry/index.sh
@@ -16,6 +16,6 @@ DIRECTORY_PATH="${PAGES_PATH}/${ENTRY_DIRECTORY}"
 ENTRY_FILE="${DIRECTORY_PATH}/index.md"
 
 /bin/mkdir $DIRECTORY_PATH
-/bin/cp $TEMPLATE_PATH $ENTRY_FILE
 
-sed -e "s/#PATH#/${ENTRY_DIRECTORY}/" -e "s/#DATE#/${DATE}/" $TEMPLATE_PATH 
+# 文字列を置換してファイルにリダイレクトする
+sed -e "s/#PATH#/${ENTRY_DIRECTORY}/" -e "s/#DATE#/${DATE}/" $TEMPLATE_PATH > $ENTRY_FILE
