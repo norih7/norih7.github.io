6a07b0e125fb44b2a4f7b4bfd7e5d21cdf2d53ad
norih <>
2019-02-18 23:16:00 +0900

    :bug: worksページのURL修正

--- packages/static/src/pages/works/index.js
+++ packages/static/src/pages/works/index.js
@@ -12,7 +12,11 @@ const Works = () => (
                     <img src="http://brush-clover.com/wp/wp-content/uploads/2019/02/brushclover.png" />
                 </figure>
                 <div className={css["description"]}>
-                    <h2>brushClover</h2>
+                    <h2>
+                        <a href="http://www.brush-clover.com/" target="_blank">
+                            brushClover
+                        </a>
+                    </h2>
                     <p>2012.02</p>
                     <p>WordPressで実装したブログ。Webエンジニアの新人時代のアウトプットとして活用していました。</p>
                 </div>
@@ -22,7 +26,11 @@ const Works = () => (
                     <img src="http://brush-clover.com/wp/wp-content/uploads/2019/02/canvas-edit.png" />
                 </figure>
                 <div className={css["description"]}>
-                    <h2>CanvasEdit</h2>
+                    <h2>
+                        <a href="http://www.brush-clover.com/canvas-edit/" target="_blank">
+                            CanvasEdit
+                        </a>
+                    </h2>
                     <p>2012.01</p>
                     <p>
                         Canvas、FileAPI、Easel.js(Create.js)を利用した簡易画像加工アプリケーション。コードは拙いですが本格的なJSアプリケーションを初めて形にしたもの。
@@ -34,7 +42,11 @@ const Works = () => (
                     <img src="http://brush-clover.com/wp/wp-content/uploads/2019/02/magicwave.png" />
                 </figure>
                 <div className={css["description"]}>
-                    <h2>MagicWave</h2>
+                    <h2>
+                        <a href="http://magic.brush-clover.com/" target="_blank">
+                            MagicWave
+                        </a>
+                    </h2>
                     <p>2004.02</p>
                     <p>
                         学生の時から趣味でやっているゲーム攻略サイト。社会人になってからオリジナルのPHPフレームワークを実装して稼働中。記事はmarkdownに対応している。
