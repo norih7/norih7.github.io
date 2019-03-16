a3c4b3377947046658da9c68f2bfb08d7075f7bb
norih <goldenbell225@gmail.com>
2019-03-16 17:02:21 +0900

    WIP: storybook5、webpack4アップデート対応

--- packages/common/components/atoms/Knob/index.stories.js
+++ packages/common/components/atoms/Knob/index.stories.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, object, select } from "@storybook/addon-knobs";
+// import { withKnobs, object, select } from "@storybook/addon-knobs";
 import Knob from "../Knob";
 
-const story = storiesOf("atoms", module).addDecorator(withKnobs);
+const story = storiesOf("atoms", module);
 
 const config = {
-    skinList : ["hide"]
-}
+    skinList: ["hide"]
+};
+
+// story.add("Knob", () => {
+//     const style = { width: "10px" };
+//     return <Knob skin={select("skin", config: skinList)} style={object("style", style)} />;
+// });
 
 story.add("Knob", () => {
     const style = { width: "10px" };
-    return <Knob skin={select("skin", config: skinList)} style={object("style", style)} />;
+    return <Knob skin={select("skin", config.skinList)} style={object("style", style)} />;
 });
