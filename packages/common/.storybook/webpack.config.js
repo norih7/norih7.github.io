1f61d081cf42b857731edb0c234c8c69b0e4b96d
norih <goldenbell225@gmail.com>
2019-05-26 21:36:47 +0900

    wip: storybook修正

--- packages/common/.storybook/webpack.config.js
+++ packages/common/.storybook/webpack.config.js
@@ -1,21 +1,37 @@
 const path = require("path");
 
-module.exports = {
-    plugins: [],
-    module: {
-        rules: [
+// module.exports = {
+//     plugins: [],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 loaders: ["style-loader", "css-loader?modules"]
+//             },
+//             {
+//                 test: /\.(ts|tsx)$/,
+//                 use: [
+//                     {
+//                         loader: require.resolve("awesome-typescript-loader")
+//                     },
+//                     {
+//                         loader: require.resolve("react-docgen-typescript-loader")
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// };
+
+module.exports = ({ config }) => {
+    config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
             {
-                test: /\.css$/,
-                loaders: ["style-loader", "css-loader?modules"]
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                use: [
-                    {
-                        loader: require.resolve("awesome-typescript-loader")
-                    }
-                ]
+                loader: require.resolve("awesome-typescript-loader")
             }
         ]
-    }
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
 };
